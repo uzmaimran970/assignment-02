6f78e6130d73e9adde04b051bc4845b025ea0ea7
Uzma imran <148060110+uzmaimran970@users.noreply.github.com>
2024-03-08 16:02:27 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+1.; // Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var name1 = "Eric";
+console.log(`"Hello ${name1}, would you like to learn some python?"`);
---
+++ index.ts
@@ -0,0 +1,5 @@
+1.// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+var name1: string = "Eric"
+
+console.log(`"Hello ${name1}, would you like to learn some python?"`)
